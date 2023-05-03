5c33034e1578d045bb6fcdae551af54dbbc2f2ef
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-05-03 17:03:02 +0200

    fix of auth

--- Auth/auth.js
+++ Auth/auth.js
@@ -1,17 +1,24 @@
-const User = require("../user");
+const User = require("../model/User")
 
 exports.register = async (req, res, next) => {
-    const { username, password } = req.body;
+    const { username, password } = req.body
     if (password.length < 6) {
-        return res.status(400).json({ msg: "Password must be at least 6 characters" })
+        return res.status(400).json({ message: "Password less than 6 characters" })
     }
-
     try {
-
-    } catch (e) {
+        await User.create({
+            username,
+            password,
+        }).then(user =>
+            res.status(200).json({
+                message: "User successfully created",
+                user,
+            })
+        )
+    } catch (err) {
         res.status(401).json({
-            msg: " User not successfully added",
-            error: error.message,
+            message: "User not successful created",
+            error: error.mesage,
         })
     }
 }
